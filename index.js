238a44e2c61420a66b4f294ac761931a8804828b
HumeraKhan98 <125649594+HumeraKhan98@users.noreply.github.com>
2023-03-19 15:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ var app = express()
 const port = 3000
 
 app.get('/', function(req, res){
-  res.send('Hello World again!!');
+  res.send('Hello World on 19th of March!');
 });
 
-/* istanbul ignore next */
-/*if (!module.parent) {*/
-  app.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Express started on port 3000');
 });
